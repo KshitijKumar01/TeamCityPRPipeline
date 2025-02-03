a46a1a30d4fdaf3c7876b2f8ffa21d32b7761c92
Kshitij Kumar <86919319+KshitijKumar01@users.noreply.github.com>
2025-02-03 15:01:34 +0530

    Update update_parameters.py

--- update_parameters.py
+++ update_parameters.py
@@ -8,7 +8,18 @@ def update():
     fetcher = GitHubPRLabelsFetcher(GITHUB_REPO_OWNER, GITHUB_REPO_NAME, GITHUB_TOKEN)
     labels = fetcher.fetch_and_print_labels("1")
 
+    # Check if the labels were returned as key-value pairs
     if labels:
+        print("##teamcity[setParameter name='HAS_LABELS' value='true']")
         for key, value in labels.items():
-            if key.lower() == "pmbd_prod":
-                print(f"##teamcity[setParameter name='PMBD_PROD' value = '{value}']")
+            if key.lower() == "pmbd-prod":
+            # Set the TeamCity parameters for each value
+                print(f"##teamcity[setParameter name='PMBD_Prod' value='{value}']")
+            if key.lower() == "asw":
+            # Set the TeamCity parameters for each value
+                print(f"##teamcity[setParameter name='ASW' value='{value}']")
+            if key.lower() == "bsw":
+            # Set the TeamCity parameters for each value
+                print(f"##teamcity[setParameter name='BSW' value='{value}']")
+    else:
+        print("##teamcity[setParameter name='HAS_LABELS' value='false']")
